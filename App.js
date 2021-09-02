2c039f42fcce7ccff60f708b5ff62cb4cf95b996
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2021-09-02 04:27:54 -0700

    update::

---
+++ App.js
@@ -0,0 +1,12 @@
+var btntranslate=document.querySelector("#btntranslate");
+
+function clickhandler() {
+    console.log("clicked!");
+}
+
+btntranslate.addeventListener("click!",clickhandler);
+
+
+
+
+
