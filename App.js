0921e559600e7ca7f94c9a817a3b5f84b36cb827
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2021-09-10 03:36:21 -0700

    update:app

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
 var btntranslate=document.querySelector("#btn-translate");
-var txtinput=document.querySelector("#txt-input");
-
-
+var txtInput=document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
 
+var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=testing&=testing"
 
+//var serverURL = "https://api.funtranslations.com/translate/minion.json" 
 
-
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+function errorhandler(error) {
+    console.log("error occured",error);
+    alert("something wrong with the server")
+}
 
 
 function clickhandler() {
-    outputDiv.innerText="fbhfshbhb" + txtinput.value;
+    var inputText= txtInput.value;///taking input value
+
+    
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+    .then(json => {
+      
+        var translatedText=json.contents.translated;
+        outputDiv.innerText= translatedText;  
+    }).catch(errorhandler)
+
+
+
+
+
+   
+    
+
+  
+
+
+
+
+
+
+
+
+
 
 
 
